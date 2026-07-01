4d072981039d17f2da42e211c83cef380212d704
greybeetle213 <65577606+greybeetle213@users.noreply.github.com>
2026-07-02 01:06:51 +1200

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using LeftPoseFix;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(main), "LeftPoseFix", "1.0.0", "GreyBeetle")]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+[assembly: MelonColor(255, 195, 0, 255)]
+[assembly: MelonAuthorColor(255, 195, 0, 255)]
+[assembly: VerifyLoaderVersion(0, 6, 2, true)]
+
---
+++ main.cs
@@ -0,0 +1,43 @@
+﻿using Il2CppRUMBLE.Players.Subsystems;
+using Il2CppRUMBLE.Managers;
+using Il2CppRUMBLE.Poses;
+using MelonLoader;
+using UnityEngine;
+using RumbleModdingAPI.RMAPI;
+using static Il2CppRUMBLE.Poses.PoseSet;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+
+namespace LeftPoseFix
+{
+
+    public class main : MelonMod
+    {
+        private Il2CppSystem.Collections.Generic.List<PoseInputSource> inputSources; // the list of all poses the player has access to
+        private bool sceneLoading = true;
+        public override void OnLateInitializeMelon() {
+            Actions.onMapInitialized += (string sceneName) => {
+                inputSources = Calls.Players.GetLocalPlayerController().gameObject.transform.GetComponentInChildren<PlayerPoseSystem>().currentInputPoses;
+                sceneLoading = false;
+            };
+        }
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
+            sceneLoading = true;
+        }
+        public override void OnUpdate() {
+            if (sceneLoading == true) {
+                return;
+            }
+            //loop through all poses and see if the left handed varient is being done.
+            bool doingPose = false;
+            for (int i = 0; i < inputSources.Count; i++) {
+                Il2CppReferenceArray<PoseConfiguration> poseConfigs = inputSources[i].poseSet.configurations;
+                PoseData poseData = poseConfigs[poseConfigs.Count - 1].Pose; //The pose config will either be just one modifyer pose, or the base pose then a structure pose.
+                if (poseData.IsDoingPose(Calls.Players.GetLocalPlayerController(), PoseData.ComparisonMethod.Mirrored).IsValidPose) {
+                    doingPose = true;
+                }
+            }
+            Calls.Players.GetLocalPlayerController().PlayerAnimator.animator.SetBool(Animator.StringToHash("PoseFistsActive"), doingPose); //close hands if doing left handed pose.
+        }
+    }
+}
+
